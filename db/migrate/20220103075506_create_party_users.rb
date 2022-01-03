be5657b0a7ad9316952714b39865756e2fad4026
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-03 16:59:14 +0900

    migrateファイル記述

--- db/migrate/20220103075506_create_party_users.rb
+++ db/migrate/20220103075506_create_party_users.rb
@@ -1,7 +1,7 @@
 class CreatePartyUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :party_users do |t|
-
+      t.references :party, null: false, foreign_key: true
       t.timestamps
     end
   end
