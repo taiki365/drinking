c304199dc8157c6880cf6cfa20a72e42221c2f23
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-03 16:59:57 +0900

    中間テーブルmigrateファイル記述追加

--- db/migrate/20220103075506_create_party_users.rb
+++ db/migrate/20220103075506_create_party_users.rb
@@ -2,6 +2,7 @@ class CreatePartyUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :party_users do |t|
       t.references :party, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
