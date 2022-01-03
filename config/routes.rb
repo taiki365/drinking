f1cd6b0341a981596cf12f9d11a035be518190d0
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-03 16:52:00 +0900

    topページ作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'parties/index'
+  root to: "parties#index"
   resources :parties
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
