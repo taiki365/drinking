18f3ea647a6ff1f54248832157707d8b099aa868
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-03 17:12:16 +0900

    user modelアソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :party_users
+  has_many :party, through: :party_users
 end
